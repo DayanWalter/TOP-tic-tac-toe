24145cd65bdeb3e8f91ea7fa612f21f99cc23cb9
SynthCyraX <SynthCyraX@gmail.com>
2023-07-05 05:51:30 +0200

    Added add-function for gameBoard

--- script.js
+++ script.js
@@ -3,10 +3,11 @@
 const gameBoard = (() => {
 // Put an array as property in the object
 // fill it with an example
-    const board = ["x","o","x","o","x","o","x","o","x"];
-return {
-    board,
-};
-
+    let board = ["x","o","x","o","x","o","x","o","x"];
+    const add = (a) => board.push(a)
+    
+    return {
+        board,
+        add,
+    };
 })();
-console.log(gameBoard.board);
