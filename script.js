731b5fc9240a10280c435aad5a36e7c4baae3e62
SynthCyraX <SynthCyraX@gmail.com>
2023-07-06 05:36:01 +0200

    Restarted project

--- script.js
+++ script.js
@@ -1,37 +1,16 @@
-// create an object and call it gameBoard
-// create the object as a module
-const gameBoard = (() => {
-// Put an array as property in the object
-// fill it with an example
-    const board = [
-        [0, 0, 0],
-        [0, 0, 0],
-        [0, 0, 0]
-    ];
+function Gameboard() {
+    
+  }
 
-    return {
-        board,
-    };
-})();
+  function Cell() {
+   
 
-// add  a factory function for player creation
-const Player = (name, token) => {
-    let _getName = () => name;
-    let _getToken = () => token;
-    const addChoice = (row, column) => {
-        gameBoard.board[row][column] = token;
-        console.log(_getName() + " took " + _getToken() + " and made a choice.");
-        console.log(gameBoard.board);
-    };
-
-    return{
-        addChoice,
-    };
-}
-
-const player1 = Player("Player 1", "1");
-const player2 = Player("Player 2", "2");
-
-const displayController = (() => {
-
-})();
+  }
+  function GameController(
+    playerOneName = "Player One",
+    playerTwoName = "Player Two"
+  ) {
+    
+  }
+  
+  const game = GameController();
