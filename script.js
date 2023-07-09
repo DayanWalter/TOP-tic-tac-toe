13129156917738f1f88edf78d69be5c07a7b8b2b
SynthCyraX <SynthCyraX@gmail.com>
2023-07-09 08:41:45 +0200

    Added board array

---
+++ script.js
@@ -0,0 +1,16 @@
+const Gameboard = (()=>{
+  let board = [];
+  let rows = 3;
+  let columns = 3;
+
+for (let i = 0; i < rows; i++) {
+  board[i] = [];
+  for (let j = 0; j < columns; j++) {
+    board[i].push(0);
+  }
+}
+
+return{
+  board
+}
+})()
