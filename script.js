ff5bf0fa1e4f1e81bd4298a4d312111064db6e19
SynthCyraX <SynthCyraX@gmail.com>
2023-07-09 09:55:26 +0200

    Changed structure and moved functions

--- script.js
+++ script.js
@@ -10,36 +10,9 @@ for (let i = 0; i < rows; i++) {
     board[i].push(0);
   }
 }
-
-return{
-  board
-}
-})();
-
-// function for player creation
-const Player = (name, token) => {
-  const getName = () => name;
-  const getToken = () => token;
-
-  return {
-    getName,
-    getToken,
-  }
-}
-// 2 Players created
-let playerOne = Player("Player One", "X");
-let playerTwo = Player("Player Two", "O");
-
-// Module for controlling the flow of the game
-const GameController = (() => {
-  // function for marking the board
-  const markBoard = (player, row, column) => {
-    Gameboard.board[row][column] = player.getToken();
-  }
   const renderBoard = () => {
-    
     let table = document.getElementById("boardArray")
-    let board = Gameboard.board
+    table.innerHTML = "";
     for (let i = 0; i < board.length; i++) {
       let row = document.createElement("tr");
 
@@ -48,13 +21,75 @@ const GameController = (() => {
           cell.textContent = board[i][j];
           row.appendChild(cell);
       }
-
       table.appendChild(row);
     }
   }
+
+return{
+  board,
+  renderBoard,
+}
+})();
+
+// // function for player creation
+// const Player = (name, token) => {
+//   const getName = () => name;
+//   const getToken = () => token;
+
+//   return {
+//     getName,
+//     getToken,
+//   }
+// }
+// // 2 Players created
+// let playerOne = Player("Player One", "X");
+// let playerTwo = Player("Player Two", "O");
+
+// Module for controlling the flow of the game
+const GameController = ((
+  playerOneName = "Player One",
+  playerTwoName = "Player Two") => {
+    const board = Gameboard;
+    const players = [
+      {
+        name: playerOneName,
+        token: "X"
+      },
+      {
+        name: playerTwoName,
+        token: "O"
+      }
+    ];
+    let activePlayer = players[0];
+
+    const switchPlayer = () => {
+      activePlayer = activePlayer === players[0] ? players[1] : players[0]
+    };
+
+    const getActivePlayer = () => activePlayer;
+
+  // function for marking the board
+  const markBoard = (player, row, column) => {
+    Gameboard.board[row][column] = player.getToken();
+  }
+  // function for rendering the board to html
+  
+  // Initialize first round
+  // PlayerOne chooses one field
+  // after choosing, PlayerTwo's turn
+  const playRound = () => {
+    console.log("");
+  }
+  const playerChoose = () => {
+
+  }
+
   return{
     markBoard,
-    renderBoard,
   }
 
 })()
+Gameboard.renderBoard()
+// GameController.markBoard(playerOne,0,0)
+// GameController.renderBoard()
+
